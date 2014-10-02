9b0881d45e98ce4c5f848bdbee8f9c9e559e912f
Nicolas Trangez <ikke@nicolast.be>
2014-10-02 16:01:25 +0200

    Stash

--- src/HandlePlantUMLBlock.hs
+++ src/HandlePlantUMLBlock.hs
@@ -1,41 +1,58 @@
-import Text.Pandoc.JSON
-import Data.ByteString.Lazy (hGetContents, hPut)
-import Data.ByteString.Lazy.UTF8 (fromString)
+{-# LANGUAGE LambdaCase #-}
+
+module Main where
+
+import Control.Monad (unless)
+
+import System.IO (hClose, hPutStrLn, IOMode(..), stderr, withFile)
+import System.Exit (ExitCode(ExitSuccess), exitWith)
+import System.Process (CreateProcess(std_in, std_out), StdStream(CreatePipe, UseHandle),
+    createProcess, proc, waitForProcess)
+import System.Directory (removeFile)
+
+import qualified Data.ByteString.Lazy as BSL
+
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Encoding as TE
+
 import Data.Digest.Pure.SHA (sha1, showDigest)
-import System.IO (hClose, hPutStr, IOMode(..), openBinaryFile)
-import System.Process
+
+import Text.Pandoc.JSON (Block(CodeBlock, Para), Inline(Image), toJSONFilter)
 
 processBlocks :: Block -> IO Block
-processBlocks b =
-  case b of
-    CodeBlock (_ , ["plantuml"], _) content -> plantUMLToImg content
-    _ -> return b
+processBlocks = \case
+    CodeBlock (_ , ["plantuml"], _) content -> plantUMLToImg $ T.pack content
+    b -> return b
 
-plantUMLToImg :: String -> IO Block
-plantUMLToImg content =  do
-  path <- renderImage content
-  return $ Para [Image [] (path, "")]
+plantUMLToImg :: Text -> IO Block
+plantUMLToImg content = do
+    path <- renderImage content
+    return $ Para [Image [] (path, "")]
 
-renderImage :: String -> IO String
+renderImage :: Text -> IO FilePath
 renderImage content = do
-  let path = uniqueName content ++ ".eps"
-  (Just hIn, Just hOut, _, _) <-
-    createProcess (proc "plantuml" ["-pipe", "-teps"]){ std_in = CreatePipe,
-                                                        std_out = CreatePipe }
-  hPutStr hIn content
-  hClose hIn
+    c <- withFile path WriteMode $ \hnd -> do
+        (Just hIn, _, _, p) <-
+            createProcess (proc "plantuml" ["-pipe", "-teps"]) { std_in = CreatePipe
+                                                               , std_out = UseHandle hnd
+                                                               }
+        BSL.hPutStr hIn content'
+        hClose hIn
 
-  hFile <- openBinaryFile path WriteMode
-  img <- hGetContents hOut
-  hPut hFile img
+        waitForProcess p
 
-  hClose hFile
-  hClose hOut
+    unless (c == ExitSuccess) $ do
+        hPutStrLn stderr $ "plantuml invocation exited with: " ++ show c
+        removeFile path
 
-  return path
+        exitWith c
 
-uniqueName :: String -> String
-uniqueName = showDigest . sha1 . fromString
+    return path
+  where
+    content' = TE.encodeUtf8 content
+    uniqueName = showDigest . sha1
+    path = uniqueName content' ++ ".eps"
 
 main :: IO ()
 main = toJSONFilter processBlocks
